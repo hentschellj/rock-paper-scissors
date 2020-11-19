ecd9f51bd71b2c987fab01058794b279d96d152c
hentschellj <hentschellj@gmail.com>
2020-11-19 14:03:15 -0600

    Translated number inputs to proper selections

--- public/js/script.js
+++ public/js/script.js
@@ -6,14 +6,23 @@ const play = function() {
   const myResponse = prompt('Rock[1], Paper[2], Scissors[3]');
   const compResponse = computerSelectionGenerator();
 
-  mySelection.innerHTML = myResponse;
-  compSelection.innerHTML = compResponse;
+  mySelection.innerHTML = translator(myResponse);
+  compSelection.innerHTML = translator(compResponse);
 }
 
 const computerSelectionGenerator = function() {
   return Math.floor(Math.random() *  3) + 1;
 }
 
-// const translator = function(input) {
-//   input = parseInt(input)
-// }
+const translator = function(input) {
+  input = parseInt(input);
+  if (input === 1) {
+    return 'Rock';
+  } else if (input === 2) {
+    return 'Paper';
+  } else if (input === 3) {
+    return 'Scissors';
+  } else {
+    return 'Could not understand response';
+  }
+}

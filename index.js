215b653a30039a568bb5ce2ad2fb12d24563e273
hentschellj <hentschellj@gmail.com>
2020-11-19 13:32:35 -0600

    Set up basic html

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 const PORT = process.env.PORT || 4000;
 
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
 app.listen(PORT, () => {
   console.log(`The app is running on http://localhost:${PORT}`);
 })

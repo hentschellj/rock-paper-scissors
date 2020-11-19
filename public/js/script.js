7985f0a0216122dbcaba6190fe5c1ec89380ad2f
hentschellj <hentschellj@gmail.com>
2020-11-19 13:55:33 -0600

    Set-up player input and random number generator for computer

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,15 @@
 const play = function() {
-  console.log('Starting Game');
+  const myResponse = prompt('Rock[1], Paper[2], Scissors[3]');
+  const compResponse = computerSelectionGenerator();
+  
+  const mySelection = document.getElementById('my-selection-listing');
+  const compSelection = document.getElementById('comp-selection-listing');
+  const winner = document.getElementById('winner-listing');
+
+  mySelection.innerHTML = myResponse;
+  compSelection.innerHTML = compResponse;
+}
+
+const computerSelectionGenerator = function() {
+  return Math.floor(Math.random() *  3) + 1;
 }

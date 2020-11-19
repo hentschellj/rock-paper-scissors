ff12f24085923b8b68026fc3e84de1329c74c0d7
hentschellj <hentschellj@gmail.com>
2020-11-19 13:58:48 -0600

    Corrected minor error

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,6 @@
-const mySelection,
-      compSelection,
-      winner;
-
-mySelection = document.getElementById('my-selection-listing');
-compSelection = document.getElementById('comp-selection-listing');
-winner = document.getElementById('winner-listing');
+const mySelection   = document.getElementById('my-selection-listing'),
+      compSelection = document.getElementById('comp-selection-listing'),
+      winner        = document.getElementById('winner-listing');
 
 const play = function() {
   const myResponse = prompt('Rock[1], Paper[2], Scissors[3]');
@@ -17,3 +13,7 @@ const play = function() {
 const computerSelectionGenerator = function() {
   return Math.floor(Math.random() *  3) + 1;
 }
+
+// const translator = function(input) {
+//   input = parseInt(input)
+// }

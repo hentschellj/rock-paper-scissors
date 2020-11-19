1007f0c148ed3f67733c181d98c184023a095fa6
hentschellj <hentschellj@gmail.com>
2020-11-19 13:56:27 -0600

    Moved variables to universal

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,14 @@
+const mySelection,
+      compSelection,
+      winner;
+
+mySelection = document.getElementById('my-selection-listing');
+compSelection = document.getElementById('comp-selection-listing');
+winner = document.getElementById('winner-listing');
+
 const play = function() {
   const myResponse = prompt('Rock[1], Paper[2], Scissors[3]');
   const compResponse = computerSelectionGenerator();
-  
-  const mySelection = document.getElementById('my-selection-listing');
-  const compSelection = document.getElementById('comp-selection-listing');
-  const winner = document.getElementById('winner-listing');
 
   mySelection.innerHTML = myResponse;
   compSelection.innerHTML = compResponse;
